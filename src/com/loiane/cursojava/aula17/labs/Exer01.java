c8779ffc1b0f06b2369ca9c6bae9e548d4ab5b4c
heitormariano <heitor.eumesmo@gmail.com>
2022-07-17 16:29:30 -0300

    ajuste em resposta para atender ao exercicio proposto

--- src/com/loiane/cursojava/aula17/labs/Exer01.java
+++ src/com/loiane/cursojava/aula17/labs/Exer01.java
@@ -3,23 +3,20 @@ package com.loiane.cursojava.aula17.labs;
 import java.util.Scanner;
 
 public class Exer01 {
+	private static Scanner scan;
+	
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		boolean infovalido = false;
+		scan = new Scanner(System.in);
+		boolean valido = false;
 
 		do {
-			System.out.println("Informe login:");
-			String login = scan.next();
-
-			System.out.println("Informe senha:");
-			String senha = scan.next();
-
-			if (login.equals(senha)) {
-				System.out.println("Login igual a senha. Informe dados válidos");
-			} else {
-				infovalido = true;
+			System.out.println("Informe um valor entre 0 e 10");
+			int valor = scan.nextInt();
+			if (valor >= 0 && valor <= 10) {
+				valido = true;
+				System.out.println("Valor valido: " + valor);
 			}
-		} while (!infovalido);
-		System.out.println("FIM");
+		} while (!valido);
+		System.out.println("Fim");
 	}
 }
