015f87846f8dac9f8d229b5d923177e230f926ba
heitormariano <heitormariano.camara@gmail.com>
2024-02-09 21:16:14 -0300

    ajustes pontuais no texto (retirando acentuacao)

--- src/com/loiane/cursojava/aula17/labs/Exer15V2.java
+++ src/com/loiane/cursojava/aula17/labs/Exer15V2.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Exer15V2 {
 	public static void main(String[] args) {
-		// Fibonacci (incluindo o 0): 0, 1, 1,2,3, 5,8, 13, 21 ...
+		// Fibonacci (incluindo o numero 0): 0, 1, 1,2,3, 5,8, 13, 21 ...
 		Scanner scan = new Scanner(System.in);
 
 		int n;
@@ -12,16 +12,16 @@ public class Exer15V2 {
 		int segundo = 1;
 		int proximo;
 
-		System.out.println("Entre com n-ésimo termpo da serie de Fibonacci:");
+		System.out.println("Entre com n-esimo termo da serie de Fibonacci:");
 		n = scan.nextInt();
 
-		//primeiro número é impresso diretamente
-		//Se forem, por exemplo, 3 termos da série, será preciso descobir o segundo e terceiro (2 números faltando).
-		//Se form, por exemplo, 4 termos da série, será preciso descobrir o segundo, terceiro e quarto (3 números faltando)
+		//Primeiro numero impresso diretamente
+		//Se forem, por exemplo, 3 termos da  serie, sera preciso descobir o segundo e terceiro (2  numeros faltando).
+		//Se form, por exemplo, 4 termos da serie, sera preciso descobrir o segundo, terceiro e quarto (3 numeros faltando)
 		//E assim por diante...
 		
 		System.out.println(primeiro); 
-		for (int i = 0; i < (n - 1) ; i++) { // serão necessárias (n - 1) operações dentro do for 
+		for (int i = 0; i < (n - 1) ; i++) { // serao necessarias (n - 1) operacoes dentro do for 
 			proximo = primeiro + segundo;
 			primeiro = segundo;
 			segundo = proximo;
